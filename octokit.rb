e21fc392a106ec0160a8fe5ac68ed1da3a529433
Julian Grosshauser <julian@juliangrosshauser.com>
2017-05-25 21:56:06 +0200

    Print pull request status

--- octokit.rb
+++ octokit.rb
@@ -5,7 +5,10 @@ require "octokit"
 require "pp"
 
 REPOSITORY = "juliangrosshauser/gh_status_webhook"
+SHA = "6f40724178ef7de899e197405b3c68b230cc5933"
 
 client = Octokit::Client.new(netrc: true)
 pull_requests = client.pull_requests(REPOSITORY, state: "open")
-pull_requests.map(&:to_h).each { |pr| pp pr }
+pull_request = pull_requests.find { |pr| pr.head.sha == SHA }
+pull_request_status = client.statuses(REPOSITORY, pull_request.head.sha).first
+puts pull_request_status.state
