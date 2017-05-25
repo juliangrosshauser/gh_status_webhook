6f40724178ef7de899e197405b3c68b230cc5933
Julian Grosshauser <julian@juliangrosshauser.com>
2017-05-25 21:47:14 +0200

    Print pull request data

--- octokit.rb
+++ octokit.rb
@@ -2,8 +2,10 @@
 
 require "bundler/setup"
 require "octokit"
+require "pp"
 
 REPOSITORY = "juliangrosshauser/gh_status_webhook"
 
 client = Octokit::Client.new(netrc: true)
-puts client.pull_requests(REPOSITORY, state: "open")
+pull_requests = client.pull_requests(REPOSITORY, state: "open")
+pull_requests.map(&:to_h).each { |pr| pp pr }
